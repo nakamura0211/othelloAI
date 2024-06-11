57b44d02841ba880906748c812980baef0e6804f
nakamura0211 <kuromu0211@gmail.com>
2024-06-12 00:38:31 +0900

    なんか弱くなってたの直した

--- play/mini_max_play.py
+++ play/mini_max_play.py
@@ -1,21 +1,20 @@
-import functools
 from sys import setrecursionlimit
 from othello import Othello, fromHistory
 from play.script_play import evaluate_board
 from random import choice
-import time
+from math import inf
 
 setrecursionlimit(10**8)
 
 def mini_max_play(othello: Othello, color,depth=2):
   ps=othello.possible_puts(color)
-  best_score=-float("inf")
+  best_score=-inf
   scores=[]
   for put in ps:
     score=-mini_max(othello.history+[put],3-color,3-color,depth)
     if score>best_score:
       best_score=score
-      scores.append(score)
+    scores.append(score)
   return choice([ps[i] for i,j in enumerate(scores) if j==best_score])
 
 def mini_max_play_depth(depth:int):
@@ -31,11 +30,11 @@ def mini_max(history,origin_color,color,depth):
     else:
       return -10000
   if not othello.is_possible_to_put_anywhere(color):
-    return -mini_max(history,origin_color,3-color,depth)
+    return mini_max(history,origin_color,3-color,depth)
   if depth==0:
     return evaluate_board(othello.board,color)
   next_puts=othello.possible_puts(color)
-  best=0
+  best=-inf
   for put in next_puts:
     s=-mini_max(history+[put],origin_color,3-color,depth-1)
     if s>best:
