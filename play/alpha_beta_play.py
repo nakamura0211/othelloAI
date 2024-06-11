c0a0470a2127e3eec3a4ec40ec8d668d075641d7
nakamura0211 <kuromu0211@gmail.com>
2024-06-12 01:21:09 +0900

    infの書き方かえた

--- play/alpha_beta_play.py
+++ play/alpha_beta_play.py
@@ -1,15 +1,16 @@
 from sys import setrecursionlimit
 from othello import Othello, fromHistory
 from play.script_play import evaluate_board
+from math import inf
 
 setrecursionlimit(10**8)
 
 def alpha_beta_play(othello: Othello, color,depth=3):
   ps=othello.possible_puts(color)
   best=None
-  alpha=-float("inf")
+  alpha=-inf
   for put in ps:
-    score=-alpha_beta(othello.history+[put],3-color,3-color,depth,-float("inf"),-alpha)
+    score=-alpha_beta(othello.history+[put],3-color,3-color,depth,-inf,-alpha)
     if score>alpha:
       best=put
       alpha=score
@@ -26,7 +27,7 @@ def alpha_beta(history,origin_color,color,depth,alpha,beta):
     else:
       return -10000
   if not othello.is_possible_to_put_anywhere(color):
-    return -alpha_beta(history,origin_color,3-color,depth,alpha,beta)
+    return alpha_beta(history,origin_color,3-color,depth,alpha,beta)
   if depth==0:
     return evaluate_board(othello.board,color)
   next_puts=othello.possible_puts(color)
