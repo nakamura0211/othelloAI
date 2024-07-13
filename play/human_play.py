c650c5c6c03891fc2790af8d53f0aa93e276c0b4
yuma1228 <yuma0114n@gmail.com>
2024-07-13 18:23:04 +0900

    入力のバグを直した

--- play/human_play.py
+++ play/human_play.py
@@ -1,7 +1,14 @@
 def human_play(othello, color: int):
     x = -1
     y = -1
-    print(f"{color}")
+    print(othello.colors[color])
     while not othello.is_possible_to_put(x, y, color):
-        x, y = map(int, input().split(" "))
-    return x, y
+        s = input()
+        if not len(s) == 3 or not s[0].isdigit() or not s[1] == " " or not s[2].isdigit() :
+            continue
+        t= map(int, s.split(" "))
+        lis=list(t)
+        x=lis[0]
+        y=lis[1]
+
+    return x,y
