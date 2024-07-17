7c291a2cf58717d1c662c4f2ca05f5910d30e694
nakamura0211 <kuromu0211@gmail.com>
2024-07-17 23:16:45 +0900

    コメントたした

--- gui_othello.py
+++ gui_othello.py
@@ -23,9 +23,11 @@ class GUI_Othello(Othello):
         pos_puts = self.possible_puts(1)
         winner = -1
         end_flag = False
+        #ゲームループ
         while True:
             player = black if color == 1 else white
             screen.fill((0, 155, 0))
+            #一手前の手をハイライト
             if len(self.history) > 0:
                 x, y = self.history[-1]
                 pygame.draw.rect(
@@ -33,6 +35,7 @@ class GUI_Othello(Othello):
                     (180, 180, 0),
                     Rect(44 + 64 * x, 44 + 64 * y, 64, 64),
                 )
+            #枠線
             for i in range(9):
                 pygame.draw.line(
                     screen, (0, 0, 0), (i * 64 + 44, 44), (i * 64 + 44, 556), 1
@@ -41,7 +44,7 @@ class GUI_Othello(Othello):
                 pygame.draw.line(
                     screen, (0, 0, 0), (44, i * 64 + 44), (556, i * 64 + 44), 1
                 )
-
+            #オセロの石
             for y in range(8):
                 for x in range(8):
                     s = self.board[y][x]
@@ -49,7 +52,9 @@ class GUI_Othello(Othello):
                         continue
                     c = (0, 0, 0) if s == 1 else (255, 255, 255)
                     pygame.draw.circle(screen, c, (76 + x * 64, 76 + y * 64), 25)
+            #描画
             pygame.display.update()
+
             if player is not None:
                 x, y = player(self, color)
                 self.put(x, y, color)
@@ -69,8 +74,11 @@ class GUI_Othello(Othello):
                 if e.type == QUIT:
                     pygame.quit()
                     sys.exit()
+                #クリックされたとき
                 elif e.type == MOUSEBUTTONDOWN:
+                    #どのセルがクリックされたのか
                     x, y = map(lambda x: math.floor((x - 44) / 64), e.pos)
+                    #置けるセルなら処理する
                     if (x, y) in pos_puts:
                         self.put(x, y, color)
                         self.history.append((x, y))
@@ -82,8 +90,11 @@ class GUI_Othello(Othello):
                             winner = w
                             end_flag = True
                         pos_puts = self.possible_puts(color)
+                #マウスが動いたとき
                 elif e.type == MOUSEMOTION:
+                    #触っているセルを計算
                     x, y = map(lambda x: math.floor((x - 44) / 64), e.pos)
+                    #置けるセルなら表示を変える
                     if (x, y) in pos_puts:
                         pygame.mouse.set_cursor(pygame.cursors.diamond)
                     else:
