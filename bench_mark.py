fdcfe7ae10037b6466b89c463bcaba47c9377913
nakamura0211 <kuromu0211@gmail.com>
2024-08-12 13:28:24 +0900

    update

--- bench_mark.py
+++ bench_mark.py
@@ -1,17 +1,32 @@
 from othello import Othello
 from play.random_play import random_play
 from play.script_play import script_play
-from play.mini_max_play import mini_max_play,mini_max_play_depth
-from play.alpha_beta_play import alpha_beta_play,alpha_beta_play_depth
+from play.alpha_beta_play import alpha_beta_play
+from evaluate.evaluate_board_nkmr_es import evaluate_board_nkmr_es
 from time import perf_counter
+
+class StartsWithRandom:
+            def __init__(self,agent,random_time=5):
+                self.agent=agent
+                self.random_time=random_time
+            def __call__(self, othello,color):
+                if self.random_time==0:
+                    return self.agent(othello,color)
+                self.random_time-=1
+                return random_play(othello,color)
+
 
 
 j=0
 attempt_number=10
+
+target=evaluate_board_nkmr_es()
+opponent=StartsWithRandom(alpha_beta_play(2))
+
 start_time=perf_counter()
 for i in range(1,attempt_number+1):
   o=Othello()
-  if o.play(alpha_beta_play_depth(2),random_play,False,False)==1:
+  if o.play(target,opponent,False,False)==1:
     j+=1
   print(j/i*100)
 k=0
@@ -22,7 +37,7 @@ print("change\n")
 start_time=perf_counter()
 for i in range(1,attempt_number+1):
   o=Othello()
-  if o.play(random_play,alpha_beta_play_depth(2),False,False)==2:
+  if o.play(opponent,target,False,False)==2:
     k+=1
   print(k/i*100)
 print("勝率",j/attempt_number*100,k/attempt_number*100)
