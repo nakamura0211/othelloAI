ee37a58c606399ab7035a736ae43417cd8765d7e
yuma1228 <yuma0114n@gmail.com>
2024-10-07 18:03:26 +0900

    治せなかった

--- es_ngn.py
+++ es_ngn.py
@@ -30,33 +30,19 @@ def input_table(individual):
 def othello_eval(individual):
     global elite_individual
     global evaluation_table
-    global evaluation_table2
-
-    if elite_individual is None:
-        ageny = alpha_beta_play(0,evaluate_board_nkmr)
-        evaluation_table = input_table(individual)       
-        agent = alpha_beta_play(0,evaluate_board_ngn)
-        othello = Othello()
-        a = othello.play(agent,ageny)
-        _,b,w=othello.count()
-        res = b-w
-        if a == 1:
-            return 100+res,
-        else:
-            return res,
+    global evaluation_table2    
+    othello = Othello()
+    evaluation_table2 = input_table(elite_individual)
+    agent_1 = alpha_beta_play(0,evaluate_board_ngn2)
+    evaluation_table = input_table(individual)        
+    agent_2 = alpha_beta_play(0,evaluate_board_ngn)
+    a = othello.play(agent_2,agent_1,do_print = False)
+    _,b,w=othello.count()
+    res = b-w 
+    if a == 1:
+        return 100+res,
     else:
-        othello = Othello()
-        evaluation_table2 = input_table(elite_individual)
-        agent_1 = alpha_beta_play(0,evaluate_board_ngn2)
-        evaluation_table = input_table(individual)        
-        agent_2 = alpha_beta_play(0,evaluate_board_ngn)
-        a = othello.play(agent_2,agent_1,print = False)
-        _,b,w=othello.count()
-        res = b-w 
-        if a == 1:
-            return 100+res,
-        else:
-            return res,
+        return res,
 
 elite_first_element = []
 elite_individual = None
@@ -74,22 +60,21 @@ toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=0.5, indpb=0.6)
 toolbox.register("select", tools.selTournament, tournsize=3)
 
 
-population = toolbox.population(n=10)
+population = toolbox.population(n=5)
 
-elite_size = 1
 
-num_generations = 200
+num_generations = 20
 for gen in range(num_generations):
     
-    elite = tools.selBest(population, elite_size)
+    
     
     if elite_individual is not None:
         elite_first_element.append(elite_individual[0])
-    elite_individual = list(elite[0])
+    elite_individual = list(tools.selBest(population, 1)[0])
     
 
     
-    offspring = toolbox.select(population, len(population) - elite_size)
+    offspring = toolbox.select(population, len(population) - 1)
     offspring = list(map(toolbox.clone, offspring))
 
     
@@ -106,7 +91,7 @@ for gen in range(num_generations):
             del mutant.fitness.values
 
 
-    offspring.extend(elite)
+    offspring.extend(list(tools.selBest(population, 1)))
 
     fitnesses = list(map(toolbox.evaluate, offspring))
     for ind, fit in zip(offspring, fitnesses):
