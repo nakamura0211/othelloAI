05998ccf3ba9857bf47bfa4f49ba95d69effbe1e
nakamura0211 <kuromu0211@gmail.com>
2024-11-06 23:13:41 +0900

    update

--- train.py
+++ train.py
@@ -13,14 +13,14 @@ import sys
 
 def train():
     num_cpus = 7
-    ray.init(num_cpus=num_cpus)
-    global_memory = SegmentMemory(50000, 4)  # Memory(50000)
+    ray.init(num_cpus=num_cpus, object_store_memory=1024 * 1024 * 1024 * 10)
+    global_memory = SegmentMemory(80000, 4)  # Memory(50000)
     agent = DqnAgent()
     n_episodes = 10000
     each_episodes = 50
     batch_size = 1024
     n_parallel_selfplay = num_cpus
-    current_weights = ray.put(agent.model.get_weights())
+    current_weights = agent.model.get_weights()
     work_in_progresses = [
         self_play.remote(agent.epsilon, current_weights, each_episodes)
         for _ in range(n_parallel_selfplay)
@@ -37,7 +37,7 @@ def train():
             if global_memory.length() > batch_size:
                 batch = global_memory.sample(batch_size)
                 agent.train(batch)
-                current_weights = ray.put(agent.model.get_weights())
+                current_weights = agent.model.get_weights()
         agent.sync_network()
         try:
             agent.model.save(f"/content/drive/My Drive/Colab Notebooks/dqn.keras")
@@ -63,35 +63,6 @@ def self_play(epsilon: int, weights: list, play_num):
                 break
 
     return memory
-
-    nx_state = []
-    cur_state = []
-    for exp in memory:
-        nx_state.append(exp.next_state.to_image())
-        cur_state.append(exp.state.to_image())
-
-    nx_pl = agent.model.predict(np.array(nx_state), verbose=0)
-    nx_tgt_pl = agent.target.predict(np.array(nx_state), verbose=0)
-    cur_pl = agent.model.predict(np.array(cur_state), verbose=0)
-
-    """td_err = []
-    for i in range(len(memory)):
-        valid = {a.index for a in OthelloEnv.valid_actions(memory[i].next_state)}
-
-        act = np.argmax([v if i not in valid else -2 for i, v in enumerate(nx_pl[i])])
-        if memory[i].state.color == memory[i].next_state.color:
-            td_err.append(
-                memory[i].reward
-                + agent.gamma * nx_tgt_pl[i][act]
-                - cur_pl[i][memory[i].action.index]
-            )
-        else:
-            td_err.append(
-                memory[i].reward
-                - agent.gamma * nx_tgt_pl[i][act]
-                - cur_pl[i][memory[i].action.index]
-            )"""
-    return memory, td_err
 
 
 if __name__ == "__main__":
